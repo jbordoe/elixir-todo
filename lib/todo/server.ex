e24bea8c7fa7ed499f791ff2bc65f39610efa566
Jesse Bordoe <jesse.bordoe@gmail.com>
2025-04-29 22:04:20 +0200

    add `Todo.{List, Server}`

---
+++ lib/todo/server.ex
@@ -0,0 +1,37 @@
+defmodule Todo.Server do
+  def start do
+    spawn(fn -> loop(Todo.List.new) end)
+  end
+
+  def add_entry(todo_list, new_entry) do
+    send(todo_list, {:add_entry, new_entry})
+  end
+  
+  def entries(todo_server, date) do
+    send(todo_server, {:entries, self(), date})
+    receive do
+      {:todo_entries, entries} -> entries
+    after 5000 ->
+      {:error, :timeout}
+    end
+  end
+
+  defp process_message(todo_list, {:add_entry, new_entry}) do
+    Todo.List.add_entry(todo_list, new_entry)
+  end
+
+  defp process_message(todo_list, {:entries, caller, date}) do
+    entries = Todo.List.entries(todo_list, date)
+    send(caller, {:todo_entries, entries})
+    todo_list
+  end
+
+  defp loop(todo_list) do
+    new_todo_list = receive do
+      message ->
+        process_message(todo_list, message)
+    end
+
+    loop(new_todo_list)
+  end
+end 
