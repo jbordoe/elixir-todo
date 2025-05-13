2efa7debbeb4f94f1d158e263ca79389c9a4fd4d
Jesse Bordoe <jesse.bordoe@gmail.com>
2025-05-13 17:46:51 +0200

    use GenServer in Todo.Server

--- lib/todo/server.ex
+++ lib/todo/server.ex
@@ -2,50 +2,54 @@ defmodule Todo.Server do
   @moduledoc """
   The server process for the todo application.
   """
+  use GenServer
 
   @process_name :todo_server
 
   ## Interface Functions
   def start do
-    pid = ServerProcess.start(Todo.Server)
-    Process.register(pid, @process_name)
+    GenServer.start(Todo.Server, [], name: @process_name)
   end
 
   def add_entry(new_entry) do
-    ServerProcess.cast(@process_name, {:add_entry, new_entry})
+    GenServer.cast(@process_name, {:add_entry, new_entry})
   end
 
   def delete_entry(entry_id) do
-    ServerProcess.cast(@process_name, {:delete_entry, entry_id})
+    GenServer.cast(@process_name, {:delete_entry, entry_id})
   end
 
   def update_entry(entry_id, updater_fun) do
-    ServerProcess.cast(@process_name, {:update_entry, entry_id, updater_fun})
+    GenServer.cast(@process_name, {:update_entry, entry_id, updater_fun})
   end
 
   def entries(date) do
-    ServerProcess.call(@process_name, {:entries, self(), date})
+    GenServer.call(@process_name, {:entries, date})
   end
 
   ## Callbacks
-  def init, do: Todo.List.new()
+  def init(_), do: {:ok, Todo.List.new()}
 
   def handle_cast({:add_entry, new_entry}, todo_list) do
-    Todo.List.add_entry(todo_list, new_entry)
+    new_state = Todo.List.add_entry(todo_list, new_entry)
+    {:noreply, new_state}
   end
 
   def handle_cast({:delete_entry, entry_id}, todo_list) do
-    Todo.List.delete_entry(todo_list, entry_id)
+    new_state = Todo.List.delete_entry(todo_list, entry_id)
+    {:noreply, new_state}
   end
 
   def handle_cast({:update_entry, entry_id, updater_fun}, todo_list) do
-    Todo.List.update_entry(todo_list, entry_id, updater_fun)
+    new_state = Todo.List.update_entry(todo_list, entry_id, updater_fun)
+    {:noreply, new_state}
   end
 
-  def handle_call({:entries, _caller, date}, todo_list) do
+  def handle_call({:entries, date}, _caller, todo_list) do
     entries = Todo.List.entries(todo_list, date)
-    {entries, todo_list}
+    {:reply, entries, todo_list}
   end
 
   # TODO: log unsupported messages
+  def handle_info(_, state), do: {:noreply, state}
 end
