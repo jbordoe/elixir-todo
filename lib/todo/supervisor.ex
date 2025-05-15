165b0f5282fb4e6a662e10d516bfe87bce998542
Jesse Bordoe <jesse.bordoe@gmail.com>
2025-05-15 21:11:12 +0200

    add db and registry to main supervisor's children

--- lib/todo/supervisor.ex
+++ lib/todo/supervisor.ex
@@ -11,6 +11,18 @@ defmodule Todo.Supervisor do
   def init(_) do
     children = [
       %{
+        id: Todo.ProcessRegistry,
+        start: {Todo.ProcessRegistry, :start_link, []},
+        restart: :permanent,
+        type: :worker
+      },
+      %{
+        id: Todo.Database,
+        start: {Todo.Database, :start_link, ["./persist"]},
+        restart: :permanent,
+        type: :supervisor
+      },
+      %{
         id: Todo.Cache,
         start: {Todo.Cache, :start_link, []},
         restart: :permanent,
